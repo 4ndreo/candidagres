06b7a0e5b24a2a108a61bd5d3f2c62a4dfa68260
ronigueiser <roni.gueiser@davinci.edu.ar>
2022-10-22 19:15:06 -0300

    elimine alkgo que no usabamos todavia que cree yo

--- src/components/inscripcion/CreateInscripcion.jsx
+++ src/components/inscripcion/CreateInscripcion.jsx
@@ -21,46 +21,5 @@ export function CreateInscripcion({ title }) {
             .catch((err) => setError(err.message));
     }
 
-    return (
-        <main className="container edit-cont">
-            <h1>Crear - {title}</h1>
-            <form onSubmit={handleSubmit}>
-                <div class="mb-3">
-                    <label className="form-label">Ingrese el nombre del curso</label>
-                    <input
-                        type="text"
-                        required
-                        onChange={(e) => setNombre(e.target.value)}
-                        className="form-control"
-                    />
-                </div>
-                <div class="mb-3">
-                    <label className="form-label">Ingrese la descripción del curso</label>
-                    <input
-                        type="text"
-                        required
-                        onChange={(e) => setDescripcion(e.target.value)}
-                        className="form-control"
-                    />
-                </div>
-                <div class="mb-3">
-                    <label className="form-label">A que hora comienza el curso</label>
-                    <input
-                        type="number"
-                        defaultValue={0}
-                        required
-                        onChange={(e) => setDuracion(parseInt(e.target.value))}
-                        className="form-control"
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary">
-                    Crear
-                </button>
-            </form>
-
-            <p>{nombre}</p>
-            <p>{duracion}</p>
-            {error && <p>{error}</p>}
-        </main>
-    );
+    
 }
