c7315a50d3d8fe34eba16d3e9dbbc5944cf288fa
4ndreo <franjandreo@gmail.com>
2022-11-15 23:13:08 -0300

    ajustes gets

--- src/components/turnos/VerTurnos.jsx
+++ src/components/turnos/VerTurnos.jsx
@@ -1,42 +1,65 @@
-import "../../pages/css/Turnos.css"
+import "../../pages/css/Turnos.css";
 
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import * as turnosService from "../../services/turnos.service";
+import * as cursosService from "../../services/cursos.service";
+import Loader from "../basics/Loader";
 
 export function VerTurnos() {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const [turnos, setTurnos] = useState([]);
-    const params = useParams();
+  const [turnos, setTurnos] = useState([]);
+  const [curso, setCurso] = useState();
+  const [error, setError] = useState([]);
+  const params = useParams();
 
-    useEffect(() => {
-        turnosService.find().then((data) => {
-            setTurnos(data);
-            // console.log(data)
-            // console.log(params?.idCurso)
-        })}, []);
-
-
-    return (
-        <main className="container main">
-            <div className="cont-turno">
-                <h1>Turnos disponibles</h1>
-                <ul>
-                    {turnos.map((turno) => {
-                         if (turno.idCurso === params?.idCurso){
-                        // console.log(params)
-                        return (
-                            <li key={turno._id}>
-                                <p>
-                                    Turno: {turno.dia} / Horario: {turno.horario}
-                                </p>
-                                <Link to={"/id-" + turno._id + "/curso/id-" + turno.idCurso}>Inscribirse</Link>
-                            </li>
-                        );}
-                    })}
-                </ul>
-            </div>
-        </main>
-    );
+  useEffect(() => {
+    turnosService.find().then((data) => {
+      setTurnos(data);
+      cursosService
+        .findById(params?.idCurso)
+        .then((curso) => {
+          setCurso(curso);
+        })
+        .catch((err) => setError(err.message));
+      // console.log(data)
+      // console.log(params?.idCurso)
+    });
+  }, []);
+if(curso && turnos.length > 0) {
+  return (
+    <main className="container main">
+      <div className="cont-turno">
+        <h1>Turnos disponibles para el curso "{curso.nombre}"</h1>
+        <ul>
+          {turnos.map((turno) => {
+            if (turno.idCurso === params?.idCurso) {
+              return (
+                <li key={turno._id}>
+                  <p>Dia del turno: {turno.dia}</p>
+                  <p>
+                    Horario: de {turno.horarioInicio}hs a {turno.horarioFin}hs
+                  </p>
+                  <Link
+                    to={"/id-" + turno._id + "/curso/id-" + turno.idCurso}
+                    className="btn btn-primary"
+                  >
+                    Inscribirse en este horario
+                  </Link>
+                </li>
+              );
+            }
+          })}
+        </ul>
+      </div>
+    </main>
+  );
+        } else {
+            return (
+                <main className="container">
+                    <Loader></Loader>
+                </main>
+            );
+        }
 }
