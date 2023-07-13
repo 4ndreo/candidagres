3578b4beff57e1316ae196aa5f9b1d573081955a
dreo <79935220+4ndreo@users.noreply.github.com>
2023-07-13 00:02:02 -0300

    feature: tamaño y distancia turnos

--- src/components/tarjeta-turno/TarjetaTurno.jsx
+++ src/components/tarjeta-turno/TarjetaTurno.jsx
@@ -1,9 +1,32 @@
+import "./TarjetaTurno.css";
+
 import React, { useEffect, useState } from "react";
+import { DateTime, Duration } from "luxon";
 
-export default function TarjetaTurno({ turno }) {
-  
+export default function TarjetaTurno({ turno, horInicio, horFin }) {
+  const factor = 50;
 
+  function getDistancia(horaInicio, horaFin) {
+    let start = DateTime.fromFormat(horaInicio, "HH:mm");
+    let end = DateTime.fromFormat(horaFin, "HH:mm");
+    let diff = end.diff(start, "hours", "minutes").toFormat("hh:mm"); 
+
+    let horas = DateTime.fromFormat(diff, "HH:mm").toFormat("HH");
+    let minutos = DateTime.fromFormat(diff, "HH:mm").toFormat("mm");
+
+    return parseInt(horas) + parseFloat(minutos) / 60;
+  }
   return (
-    <li className="item-dia w-100">{turno.nombre}</li>
+    <li
+      className="item-turno w-100"
+      style={{ height: getDistancia(horInicio, horFin) * factor + "px",
+      marginTop: getDistancia("09:00", horInicio) * factor + "px"
+    }}
+    >
+      {turno.nombre} | {
+      "Altura:" + getDistancia(horInicio, horFin) * factor} | {"Distancia:" + getDistancia("09:00", horInicio)} | 
+      {"Factor:" + factor} |
+      {"Hora Inicio:" + horInicio} |
+    </li>
   );
 }
