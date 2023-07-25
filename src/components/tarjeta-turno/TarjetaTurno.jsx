5264b0c715b8c7016db0120ce85b8564dfd53274
dreo <79935220+4ndreo@users.noreply.github.com>
2023-07-24 23:23:18 -0300

    feat(Turnos): tarjetas turnos desplegable

--- src/components/tarjeta-turno/TarjetaTurno.jsx
+++ src/components/tarjeta-turno/TarjetaTurno.jsx
@@ -2,33 +2,70 @@ import "./TarjetaTurno.css";
 
 import React, { useEffect, useState } from "react";
 import { DateTime, Duration } from "luxon";
+import { useNavigate, useParams, Link } from "react-router-dom";
 
-export default function TarjetaTurno({ turno, horInicio, horFin, color }) {
+export default function TarjetaTurno({
+  turno,
+  horInicio,
+  horFin,
+  color,
+  selectedTurno,
+  handleSelectedTurno,
+}) {
   const factor = 50;
 
   function getDistancia(horaInicio, horaFin) {
     let start = DateTime.fromFormat(horaInicio, "HH:mm");
     let end = DateTime.fromFormat(horaFin, "HH:mm");
-    let diff = end.diff(start, "hours", "minutes").toFormat("hh:mm"); 
+    let diff = end.diff(start, "hours", "minutes").toFormat("hh:mm");
 
     let horas = DateTime.fromFormat(diff, "HH:mm").toFormat("HH");
     let minutos = DateTime.fromFormat(diff, "HH:mm").toFormat("mm");
 
     return parseInt(horas) + parseFloat(minutos) / 60;
   }
-  return (
-    <li
-      className="item-turno w-100"
-      style={{ height: getDistancia(horInicio, horFin) * factor + "px",
-      top: getDistancia("09:00", horInicio) * factor + "px",
-      border: "2px solid "+color,
-      
-    }}
-    >
-      <h3>{turno.nombre}</h3>
-      {/* {"Altura:" + getDistancia(horInicio, horFin) * factor} | {"Distancia:" + getDistancia("09:00", horInicio)} | 
-      {"Factor:" + factor} | */}
-      <p>{horInicio}-{horFin}hs</p>
-    </li>
-  );
+  if (selectedTurno === turno._id) {
+    return (
+      <li
+        className="item-turno w-100"
+        style={{
+          height: "auto",
+          minHeight: getDistancia(horInicio, horFin) * factor + "px",
+          top: getDistancia("09:00", horInicio) * factor + "px",
+          border: "2px solid " + color,
+          backgroundColor: color,
+          marginBottom: "2rem"
+        }}
+        onClick={() => handleSelectedTurno(turno._id)}
+      >
+        <h3>{turno.nombre}</h3>
+        <p>
+          {horInicio}-{horFin}hs
+        </p>
+        <Link
+          to={"/id-" + turno._id + "/curso/id-" + turno.idCurso}
+          className="btn btn-primary"
+        >
+          Inscribirse en este horario
+        </Link>
+      </li>
+    );
+  } else {
+    return (
+      <li
+        className="item-turno w-100"
+        style={{
+          height: getDistancia(horInicio, horFin) * factor + "px",
+          top: getDistancia("09:00", horInicio) * factor + "px",
+          border: "2px solid " + color,
+        }}
+        onClick={() => handleSelectedTurno(turno._id)}
+      >
+        <h3>{turno.nombre}</h3>
+        <p>
+          {horInicio}-{horFin}hs
+        </p>
+      </li>
+    );
+  }
 }
