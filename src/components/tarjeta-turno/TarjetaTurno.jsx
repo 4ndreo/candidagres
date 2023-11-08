36d704ab64dc574f040024d978cefc048ea48d30
dreo <79935220+4ndreo@users.noreply.github.com>
2023-11-07 22:24:57 -0300

    feat: previsualizar seleccion de turno

--- src/components/tarjeta-turno/TarjetaTurno.jsx
+++ src/components/tarjeta-turno/TarjetaTurno.jsx
@@ -14,7 +14,8 @@ export default function TarjetaTurno({
   handleMouseOver,
   handleMouseLeave,
   hoveredTurno,
-  handleShow
+  handleShow,
+  verifyInscripto,
 }) {
   const factor = 50;
   const [tooltipState, setTooltipState] = useState(false);
@@ -61,7 +62,7 @@ export default function TarjetaTurno({
   // } else {
     return (
       <div
-        className={hoveredTurno === turno._id ? "item-turno w-100 hoveredTurno" : "item-turno w-100"}
+        className={ verifyInscripto(turno._id).some(val => val) ? "item-turno w-100 inscripto" : "item-turno w-100" }
         style={{
           height: getDistancia(horInicio, horFin) * factor + "px",
           top: getDistancia("09:00", horInicio) * factor + "px",
@@ -77,6 +78,11 @@ export default function TarjetaTurno({
         <p>
           {horInicio}-{horFin}hs
         </p>
+        {verifyInscripto(turno._id).some(val => val) ?
+        <div className="align-middle text-end">
+          <svg className="me-1" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path fill="currentColor" d="m10.6 16.6l7.05-7.05l-1.4-1.4l-5.65 5.65l-2.85-2.85l-1.4 1.4l4.25 4.25ZM12 22q-2.075 0-3.9-.788t-3.175-2.137q-1.35-1.35-2.137-3.175T2 12q0-2.075.788-3.9t2.137-3.175q1.35-1.35 3.175-2.137T12 2q2.075 0 3.9.788t3.175 2.137q1.35 1.35 2.138 3.175T22 12q0 2.075-.788 3.9t-2.137 3.175q-1.35 1.35-3.175 2.138T12 22Z"/></svg>
+          <span>Inscripto</span>
+        </div>: null }
       </div>
     );
   // }
--- src/components/turnos/VerTurnos.jsx
+++ src/components/turnos/VerTurnos.jsx
@@ -4,16 +4,21 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams, Link } from "react-router-dom";
 import * as turnosService from "../../services/turnos.service";
 import * as cursosService from "../../services/cursos.service";
+import * as inscripcionesService from "../../services/inscripciones.service";
 import Loader from "../basics/Loader";
 import TarjetaTurno from "../tarjeta-turno/TarjetaTurno";
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { useContext } from "react";
+import { AuthContext } from "../../App";
 
 export function VerTurnos() {
   let navigate = useNavigate();
+  const value = useContext(AuthContext);
 
   const [turnos, setTurnos] = useState([]);
   const [curso, setCurso] = useState();
+  const [inscripciones, setInscripciones] = useState([]);
   const [error, setError] = useState([]);
   const [selectedTurno, setSelectedTurno] = useState({});
   const [hoveredTurno, setHoveredTurno] = useState("");
@@ -50,15 +55,23 @@ export function VerTurnos() {
   const params = useParams();
 
   useEffect(() => {
-    turnosService.find().then((data) => {
+      getTurnos();
+      getInscripcionesByUser(value.currentUser._id);
+  }, []);
+  
+  function getTurnos(curso_id) {
+    turnosService.find()
+    .then((data) => {
       setTurnos(
         data?.sort(function (a, b) {
           return a.horarioInicio - b.horarioInicio;
         })
-      );
-      getCurso();
-    });
-  }, []);
+        )
+        getCurso();
+        
+    })
+    .catch((err) => setError(err.message));
+  }
 
   function getCurso(curso_id) {
     cursosService
@@ -68,6 +81,24 @@ export function VerTurnos() {
     })
     .catch((err) => setError(err.message));
   // console.log(params?.idCurso)
+  }
+
+  function getInscripcionesByUser(user_id) {
+    inscripcionesService
+    .findByUser(user_id)
+    .then((inscripciones) => {
+      setInscripciones(inscripciones);
+      // if(inscripciones.length > 0) {
+        console.log(verifyInscripto('63743748932195c12d9856f0'));
+
+      // }
+      // verifyInscripto(inscripciones);
+    })
+    .then((res) => {
+
+    }
+    )
+    .catch((err) => setError(err.message));
   }
 
   function handleSelectedTurno(turno) {
@@ -81,6 +112,16 @@ export function VerTurnos() {
 
   function handleMouseLeave() {
     setHoveredTurno('');
+  }
+
+  function verifyInscripto(turno_id) {
+    console.log(inscripciones)
+    return inscripciones.map(inscripcion =>
+      // {
+      // console.log(inscripcioness)
+      Object.values(inscripcion).some((arrVal) => turno_id === arrVal)
+    // }
+    )
   }
 
   if (curso && turnos.length > 0) {
@@ -109,6 +150,7 @@ export function VerTurnos() {
                             handleMouseLeave={handleMouseLeave}
                             hoveredTurno={hoveredTurno}
                             handleShow={handleShow}
+                            verifyInscripto={verifyInscripto}
                           />
                         );
                       }
@@ -131,7 +173,7 @@ export function VerTurnos() {
             
             daysCount++
             return (
-              <span>{daysCount > 1 ? ', ' : ''} {diasSemana.find(o => o.id === dia).nombre}</span>
+              <span key={dia}>{daysCount > 1 ? ', ' : ''} {diasSemana.find(o => o.id === dia).nombre}</span>
               
               )
           })}
@@ -144,12 +186,30 @@ export function VerTurnos() {
         <Button className="btn-close-link" variant="link" onClick={handleClose}>
             Cerrar
           </Button>
-          <Link
-          to={"/id-" + selectedTurno + "/curso/id-" + curso._id}
-          className="btn btn-primary"
-        >
-          Inscribirse en este horario
-        </Link>
+          {verifyInscripto(selectedTurno._id).some(val => val) ? 
+          //  <div className="btn-group dropup" role="group">
+          //  <button type="button" className="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+          //    Dropdown
+          //  </button>
+          //  <ul className="dropdown-menu">
+          //    <li>
+            <Link
+            to={"/id-" + selectedTurno._id + "/curso/id-" + curso._id}
+            className="btn-close-link btn-inscripto">
+              
+            </Link>
+        //     </li>
+        //      {/* <li><a className="dropdown-item" href="#">Dropdown link</a></li> */}
+        //    </ul>
+        //  </div>
+          : 
+            <Link
+            to={"/id-" + selectedTurno._id + "/curso/id-" + curso._id}
+            className="btn btn-primary"
+            >
+            Inscribirse en este horario
+          </Link>
+        }
         </Modal.Footer>
       </Modal>
         </div>
