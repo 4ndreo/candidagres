af7b14ed67bb21a634a5bb221887a76dfd5e49e9
ronigueiser <roni.gueiser@davinci.edu.ar>
2023-12-11 22:41:38 -0300

    Terminado-abm-turnos

--- src/components/turnos/EditTurno.jsx
+++ src/components/turnos/EditTurno.jsx
@@ -8,9 +8,17 @@ import { AuthContext } from "../../App";
 export function EditTurno({ title }) {
   let navigate = useNavigate();
 
-  const [dia, setDia] = useState("");
-  const [horarioInicio, setHorarioInicio] = useState();
-  const [horarioFin, setHorarioFin] = useState();
+  const [hora, setHora] = useState(9);
+  const [horaFinalizar, setHoraFinalizar] = useState(10);
+  const [minutos, setMinutos] = useState("");
+  const [minutosFinalizar, setMinutosFinalizar] = useState("");
+  const [editandoHorario, setEditandoHorario] = useState(false);
+  const [modificar, setModificar] = useState(false);
+  const [dias, setDias] = useState([]);
+  const [horarioInicio, setHorarioInicio] = useState("");
+  const [horarioFin, setHorarioFin] = useState("");
+  const [nombre, setNombre] = useState();
+  const [mensajeError, setMensajeError] = useState('');
   const [icons, setIcons] = useState([]);
   const [error, setError] = useState("");
   const [checked, setChecked] = useState({});
@@ -22,9 +30,10 @@ export function EditTurno({ title }) {
     turnosService
       .findById(params?.idTurno)
       .then((turno) => {
-        setDia(turno.dia);
+        setDias(turno.dias);
         setHorarioInicio(turno.horarioInicio);
         setHorarioFin(turno.horarioFin);
+        setNombre(turno.nombre);
       })
       .catch((err) => setError(err.message));
 
@@ -35,47 +44,198 @@ export function EditTurno({ title }) {
 
   function handleSubmit(e) {
     e.preventDefault();
-    turnosService.update(params?.idTurno, { dia, horarioInicio, horarioFin }).then((data) => {
-      navigate("/panel/turnos", { replace: true });
-    });
+
+
+
+    const horarioInicio = hora + ':' + minutos
+    const horarioFin = horaFinalizar + ':' + minutosFinalizar
+
+    console.log(nombre, dias, horarioInicio, horarioFin)
+
+    if (modificar){
+      turnosService.update(params?.idTurno, { dias, horarioInicio, horarioFin }).then((data) => {
+        navigate("/panel/turnos", { replace: true });
+      });
+    } else {
+      window.alert("El horario de comienzo no puede ser menor al de finalizar")
+    }
+
+  }
+
+  function handleEditarDias(){
+    setEditandoHorario(true);
+  }
+  function handleCancelarEdicion (){
+    setEditandoHorario(false);
+  }
+
+  function handleDia(dias){
+
+    const diasMapping = {
+      D1: 'Lunes',
+      D2: 'Martes',
+      D3: 'Miércoles',
+      D4: 'Jueves',
+      D5: 'Viernes',
+      // Puedes agregar más abreviaturas y nombres según sea necesario
+    };
+
+    const nombresDias = dias.map(abreviatura => diasMapping[abreviatura]);
+
+    return nombresDias
+  }
+
+  function handleDiasChange (e) {
+    // Obtener las opciones seleccionadas
+
+    // console.log(e)
+    const opcionesSeleccionadas = Array.from(e.target.selectedOptions, (option) => option.value);
+
+    // Actualizar el estado con las opciones seleccionadas
+    setDias(opcionesSeleccionadas);
+  }
+
+  function handleHoraChange(hora){
+    setHora(hora)
+  }
+
+  function handleMinutosChange(minutos){
+
+    if (minutos < 10){
+      const newMinutos = "0" + minutos
+      console.log(newMinutos.toString())
+      setMinutos(newMinutos.toString())
+    } else {
+      console.log(hora.toString())
+      const newMinutos = minutos.toString()
+      setMinutos(newMinutos)
+    }
+  }
+  function handleHoraFinalizarChange(horaFinalizar){
+
+
+    if (hora >= horaFinalizar){
+      console.log(hora, horaFinalizar)
+      // window.alert("el horario de finalizacion no puede ser menor al de inicio")
+      setMensajeError("La hora de finalización no puede ser menor o igual a la de inicio");
+      setModificar(false)
+    } else {
+      console.log(horaFinalizar.toString())
+      const newHora = horaFinalizar.toString()
+      setMensajeError('');
+      setHoraFinalizar(newHora)
+      setModificar(true)
+    }
+
+  }
+  function handleMinutosFinalizarChange(minutos){
+
+    if (minutos < 10){
+      const newMinutos = "0" + minutos
+      console.log(newMinutos.toString())
+      setMinutosFinalizar(newMinutos.toString())
+    } else {
+      console.log(hora.toString())
+      const newMinutos = minutos.toString()
+      setMinutosFinalizar(newMinutos)
+    }
   }
 
   return (
     <main className="container edit-cont">
       <h1>Editar - {title}</h1>
       <form onSubmit={handleSubmit}>
+
+
         <div className="mb-3">
-          <label className="form-label">Ingrese el dia del turno</label>
+          <label className="form-label">Ingrese el nombre del Turno</label>
           <input
-            type="text"
-            defaultValue={dia}
-            required
-            onChange={(e) => setDia(e.target.value)}
-            className="form-control"
+              type="text"
+              defaultValue={nombre}
+              required
+              onChange={(e) => setNombre(e.target.value)}
+              className="form-control"
           />
         </div>
+
+
+        <div>
+          <p>Dias del turno: {handleDia(dias).join(', ')}</p>
+
+              <div className="mb-3">
+                <label className="form-label">Ingrese el día del turno</label>
+                <select
+                    value={dias}
+                    onChange={handleDiasChange}
+                    className="form-control"
+                    required
+                    multiple
+                >
+                  <option value="D1">Lunes</option>
+                  <option value="D2">Martes</option>
+                  <option value="D3">Miércoles</option>
+                  <option value="D4">Jueves</option>
+                  <option value="D5">Viernes</option>
+                </select>
+              </div>
+
+        </div>
+
+        <div>
+
+          <p>El turno comienza a las: {horarioInicio} hrs y termina a las {horarioFin} hrs</p>
+
+          {editandoHorario ? (
         <div className="mb-3">
-        <span className="form-label">El turno comenzará a las</span>
+          <span className="form-label">El turno comenzará a las</span>
           <input
-            type="number"
-            defaultValue={horarioInicio}
-            required
-            max={18}
-            min={9}
-            onChange={(e) => setHorarioInicio(e.target.value)}
-            className="form-control input-horario"
+              type="number"
+              defaultValue={hora}
+              required
+              min={9}
+              max={23}
+              onChange={(e) =>handleHoraChange(e.target.value)}
+              className="form-control input-horario"
+          />
+          :
+          <input
+              type="number"
+              defaultValue={minutos}
+              required
+              min={0}
+              max={59}
+              onChange={(e) =>handleMinutosChange(e.target.value)}
+              className="form-control input-horario"
           />
           hs, y terminará a las
           <input
-            type="number"
-            defaultValue={horarioFin}
-            required
-            max={18}
-            min={9}
-            onChange={(e) => setHorarioFin(e.target.value)}
-            className="form-control input-horario"
+              type="number"
+              defaultValue={horaFinalizar}
+              required
+              min={10}
+              max={23}
+              onChange={(e) =>handleHoraFinalizarChange(e.target.value)}
+              className="form-control input-horario"
+          />
+          :
+          <input
+              type="number"
+              defaultValue={minutosFinalizar}
+              required
+              min={0}
+              max={59}
+              onChange={(e) =>handleMinutosFinalizarChange(e.target.value)}
+              className="form-control input-horario"
           />
           hs.
+          {mensajeError && <p style={{ color: 'red' }}>{mensajeError}</p>}
+          <div>
+            <button className="mt-2 btn btn-danger" onClick={handleCancelarEdicion}>Cerrar Edición</button>
+          </div>
+        </div>
+          ) : (
+              <button className="mb-3 btn btn-success" onClick={handleEditarDias}>Editar Horario</button>
+          )}
         </div>
         <button type="submit" className="btn btn-primary">
           Modificar
