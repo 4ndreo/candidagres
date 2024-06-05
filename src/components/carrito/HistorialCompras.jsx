368d7db292564396e8327fac71571ca3dc3800f6
dreo <79935220+4ndreo@users.noreply.github.com>
2024-06-04 22:12:23 -0300

    feat: rediseño del historial de compras

--- src/components/carrito/HistorialCompras.jsx
+++ src/components/carrito/HistorialCompras.jsx
@@ -1,8 +1,9 @@
-import "../css/Edit.css";
+import "./HistorialCompras.css";
 
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import * as carritoService from "../../services/carrito.service";
+import * as comprasService from "../../services/compras.service";
 
 import Loader from "../basics/Loader";
 import { Col, Container, Nav, Row } from "react-bootstrap";
@@ -15,8 +16,11 @@ export function HistorialCompras() {
     let navigate = useNavigate();
     const params = useParams();
 
-    const [productosComprados, setProductosComprados] = useState([]);
+    const [compras, setCompras] = useState([]);
     const [total, setTotal] = useState(0);
+    const [importeCarrito, setImporteCarrito] = useState(0);
+    const [cantidadCarrito, setCantidadCarrito] = useState(0);
+    const [demoraCarrito, setDemoraCarrito] = useState(0);
     const [nombre, setNombre] = useState("");
     const [usuarioId, setUsuarioId] = useState("");
     const [eliminadoCorrectamente, setEliminadoCorrectamente] = useState(false);
@@ -40,46 +44,105 @@ export function HistorialCompras() {
 
     }, []);
 
+    function calcularImporteCarrito(carrito) {
+        let total = 0
+        console.log('carrito', carrito)
+        carrito.forEach(producto => {
+            total += producto.precio * producto.cantidad
+        })
+
+        return total
+    }
+
+    /**
+     * Calcula el monto total del carrito. El cálculo se hace sumando el producto del precio y cantidad de cada producto.
+     *
+     * @param {Array} carrito - El arreglo de productos en el carrito.
+     * @return {void} La función no retorna ningún valor.
+     */
+    function calcularCantidadCarrito(carrito) {
+        let total = 0
+        carrito.forEach(producto => {
+            total += producto.cantidad
+        })
+
+        return total
+    }
+
+    /**
+     * Calcula la demora de la entrega de los productos. El cálculo se hace tomando la mayor de las demoras de todos los productos, y se le suma la cantidad total de items del carrito.
+     *
+     * @param {Array} carrito - El arreglo de productos en el carrito.
+     * @return {void} La función no retorna ningún valor.
+     */
+    function calcularDemora(carrito) {
+        let total = carrito.map((x) => x.demora_producto).reduce((a, b) => a > b ? a : b)
+        carrito.forEach(producto => {
+            total += producto.cantidad
+        })
+        return total
+    }
+
 
 
 
 
     function traerCarritoUsuario(idUsuario) {
+        comprasService.findByIdUser(idUsuario).then((compras) => {
+            compras.map((compra) => {
+                compra.importeTotal = calcularImporteCarrito(compra.productos)
+                compra.cantidadTotal = calcularCantidadCarrito(compra.productos)
+                compra.demoraTotal = calcularDemora(compra.productos)
+            })
+            setCompras(compras)
+            console.log(compras)
+        })
+        // carritoService.findByIdUserFinalizado(idUsuario).then((carrito) => {
 
-        carritoService.findByIdUserFinalizado(idUsuario).then((carrito) => {
-
-            console.log(carrito)
-            setProductosComprados(carrito)
-            // setTotal(carrito.total);
-            // setProductosComprar(carrito.productosComprar)
-            //
-            // console.log(carrito.total, carrito.productosComprar)
-        }).catch((err) => setError(err.message));
+        //     console.log(carrito)
+        //     setProductosComprados(carrito)
+        //     // setTotal(carrito.total);
+        //     // setProductosComprar(carrito.productosComprar)
+        //     //
+        //     // console.log(carrito.total, carrito.productosComprar)
+        // }).catch((err) => setError(err.message));
     }
 
 
 
-    if (productosComprados.length > 0) {
+    if (compras.length > 0) {
         return (
-            <div className="cont-admin-cursos">
+            <>
                 <h1>Historial de compras - {nombre}</h1>
-                <ul className="listado-cursos">
-                    {productosComprados.map((productos, index) => (
-                        <li key={`${index}`}>
-                            <p>Compra #{index + 1} / <b> Total:</b> ${productos.total}</p>
+                <div className="cont-historial">
+                    {compras.map((compra, index) => (
+                        <div key={index} className="cont-compra-fecha">
+                            <p className="fecha">{new Date(compra.created_at).toLocaleDateString()}</p>
+                            <ul className="cont-compra">
+                                <li className="item-compra">
+                                    <div className="cont-resumen">
 
-                            <ul>
-                                {productos.productosComprar.map((producto, subIndex) => (
-                                    <li key={`${index}-${subIndex}`}>
-                                        <p><b>{producto.nombre}</b></p>
-                                        <p>${producto.precio}</p>
-                                    </li>
-                                ))}
+                                        <h2>Resumen</h2>
+                                        <p><span className="negritas">Importe total:</span> ${compra.importeTotal}</p>
+                                        <p><span className="negritas">Cantidad total:</span> {compra.cantidadTotal} artículos</p>
+                                        <p><span className="negritas">Demora:</span> {compra.demoraTotal} días</p>
+                                    </div>
+
+                                    <ul className="cont-articulos">
+                                        {compra.productos.map((producto, subIndex) => (
+                                            <li key={`${index}-${subIndex}`}>
+                                                <h3>{producto.nombre}</h3>
+                                                <p>${producto.precio}</p>
+                                                <p>{producto.cantidad}</p>
+                                            </li>
+                                        ))}
+                                    </ul>
+                                </li>
                             </ul>
-                        </li>
+                        </div>
                     ))}
-                </ul>
-            </div>
+                </div>
+            </>
         );
 
     } else {
---
+++ src/services/compras.service.js
@@ -0,0 +1,69 @@
+const url = "http://localhost:2025/";
+
+async function find() {
+    return fetch(url + "api/compras", {
+        headers: {
+            'auth-token': localStorage.getItem('token')
+        }
+    }).then((response) => response.json());
+}
+
+async function findById(idCompra) {
+    return fetch(url + "api/compras/" + idCompra, {
+        headers: {
+            'auth-token': localStorage.getItem('token')
+        }
+    }).then((response) =>
+        response.json()
+    );
+}
+
+async function findByIdUser(idUser) {
+    return fetch(url + "api/compras/user/" + idUser, {
+        headers: {
+            'auth-token': localStorage.getItem('token')
+        }
+    }).then((response) =>
+        response.json()
+    );
+}
+
+async function create(data) {
+    return fetch(url + "api/compras/compra", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            'auth-token': localStorage.getItem('token')
+        },
+        body: JSON.stringify(data),
+    }).then((response) => response.json());
+}
+
+async function remove(idCompra) {
+    return fetch(url + "api/compras/" + idCompra, {
+        method: "DELETE",
+        headers: {
+            'auth-token': localStorage.getItem('token')
+        }
+    }).then((response) => response.json());
+}
+
+async function update(compra) {
+    console.log("service.compra",compra)
+    return fetch(url + "api/compras/" + compra._id, {
+        method: "PATCH",
+        headers: {
+            "Content-Type": "application/json",
+            'auth-token': localStorage.getItem('token')
+        },
+        body: JSON.stringify({ productos: compra.productos }),
+    }).then((response) => true);
+}
+
+
+export { find,
+    findById,
+    findByIdUser,
+    create,
+    remove,
+    update,};
