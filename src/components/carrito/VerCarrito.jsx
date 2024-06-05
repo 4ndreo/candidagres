f0a3ea60d503877ce69c7fc980317bc5eea01f5d
dreo <79935220+4ndreo@users.noreply.github.com>
2024-06-04 22:12:37 -0300

    fix: crear carrito y compra

--- src/components/carrito/VerCarrito.jsx
+++ src/components/carrito/VerCarrito.jsx
@@ -7,6 +7,7 @@ import "swiper/css/pagination";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import * as carritoService from "../../services/carrito.service";
+import * as comprasService from "../../services/compras.service";
 
 import Loader from "../basics/Loader";
 import { Col, Container, Nav, Row, Button, Modal, ListGroup, Card } from "react-bootstrap";
@@ -56,9 +57,11 @@ export function VerCarrito() {
         return new Promise((resolve, reject) => {
             carritoService.findByIdUser(usuarioId)
                 .then((data) => {
+                    console.log('funcioné')
                     resolve(data);
                 })
                 .catch((err) => {
+                    console.log('fallé')
                     reject(err);
                 });
         })
@@ -67,22 +70,21 @@ export function VerCarrito() {
     function loadCarrito(usuarioId) {
         return new Promise((resolve, reject) => {
             getCarritobyIdUser(usuarioId).then((data) => {
-                if (data === null) {
-                    crearCarritoParaUsuario(usuarioId)
-                    setLoadingQuantities(false)
-                } else {
+                if (!data.err) {
                     setCarrito(data)
                     calcularImporteCarrito(data.productos)
                     calcularCantidadCarrito(data.productos)
                     calcularDemora(data.productos)
                     setLoadingQuantities(false)
                     localStorage.setItem("carrito", JSON.stringify(data))
+                } else {
+                    crearCarritoParaUsuario(usuarioId)
+                    setLoadingQuantities(false)
+
                 }
-            })
-                .catch((err) => {
-                    console.error(err)
-                    reject(err);
-                });
+            }).catch((err) => {
+                console.error(err)
+            });
         })
     }
 
@@ -126,11 +128,9 @@ export function VerCarrito() {
      */
     function calcularDemora(carrito) {
         let total = carrito.map((x) => x.demora_producto).reduce((a, b) => a > b ? a : b)
-
         carrito.forEach(producto => {
             total += producto.cantidad
         })
-
         setDemoraCarrito(total)
     }
 
@@ -153,15 +153,25 @@ export function VerCarrito() {
 
     }
 
-    function handleConfirmar() {
-        carritoService.remove(carritoId).then((carrito) => {
+    async function handleConfirmar() {
+        console.log(carrito.productos.map((producto) => ({ id: producto.id, cantidad: producto.cantidad })))
 
-            setProductosComprar([])
-            setImporteCarrito(0)
-            console.log(carrito)
+        let compra = {
+            usuarioId: carrito.usuarioId,
+            carritoId: carrito._id,
+            productos: carrito.productos.map((producto) => ({ id: producto.id, cantidad: producto.cantidad })),
+        }
+
+        comprasService.create(compra).then((data) => {
+            carritoService.remove(carrito._id).then((carrito) => {
+                console.log(data)
+                setShowModal(false);
+                setShowSuccessModal(true);
+
+                loadCarrito((JSON.parse(localStorage.getItem('user')))._id)
+            }).catch((err) => setError(err.message));
         }).catch((err) => setError(err.message));
-        setShowModal(false);
-        setShowSuccessModal(true);
+
     }
 
 
@@ -232,59 +242,6 @@ export function VerCarrito() {
         return (
 
             <div className="cont-admin-cursos cont-list-carrito d-flex justify-content-between">
-                {/* <Swiper
-              slidesPerView={3.5}
-              spaceBetween={30}
-              freeMode={true}
-            //   pagination={{
-            //     clickable: true,
-            //   }}
-              modules={[FreeMode, Pagination]}
-              className="mySwiper"
-            >
-              <SwiperSlide>
-                <img
-                  className="img-bnida-swiper"
-                  src={ImagePlaceholder}
-                  alt="Taller donde se imparten las clases en Cándida Gres."
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  className="img-bnida-swiper"
-                  src={ImagePlaceholder}
-                  alt="Taller donde se imparten las clases en Cándida Gres."
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  className="img-bnida-swiper"
-                  src={ImagePlaceholder}
-                  alt="Taller donde se imparten las clases en Cándida Gres."
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  className="img-bnida-swiper"
-                  src={ImagePlaceholder}
-                  alt="Taller donde se imparten las clases en Cándida Gres."
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  className="img-bnida-swiper"
-                  src={ImagePlaceholder}
-                  alt="Taller donde se imparten las clases en Cándida Gres."
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <img
-                  className="img-bnida-swiper"
-                  src={ImagePlaceholder}
-                  alt="Taller donde se imparten las clases en Cándida Gres."
-                />
-              </SwiperSlide>
-            </Swiper> */}
                 <div className="detalle">
                     <h1>Carrito</h1>
 
@@ -343,44 +300,7 @@ export function VerCarrito() {
                             );
                         })}
                     </Swiper>
-                    {/* <ul className="listado-productos">
-                        {carrito.productos.map((producto) => {
-                            return (
-                                <li key={producto._id}>
-                                    <Card key={producto._id}>
-                                        <Card.Img className="card-img" variant="top" src={ImagePlaceholder} />
-                                        <Card.Body>
-                                            <h2 className="title">{producto.nombre}</h2>
-                                            <Link className="card_link" to={`/tienda/producto/id-${producto._id}`}></Link>
-                                            <Card.Text className="precio">
-                                                ${producto.precio}
-                                            </Card.Text>
-                                            <Card.Text>
-                                                {producto.descripcion}
-                                            </Card.Text>
-                                        </Card.Body>
-                                        <Card.Footer>
-                                            <div className="counter-cantidad btn-carrito">
-                                                <span className="icon-carrito">
-                                                </span>
-                                                <div>
-                                                    {loadingQuantities ? <LoaderMini className="loader-mini" /> :
-                                                        <>
-                                                            <Button variant="danger" onClick={() => handleSubstractItemToCart(producto._id)} disabled={loadingQuantities}>-</Button>
 
-                                                            {checkQuantity(producto._id) || 0}
-
-                                                            <Button variant="success" onClick={() => handleAddItemToCart(producto._id)} disabled={loadingQuantities}>+</Button>
-                                                        </>
-                                                    }
-                                                </div>
-                                            </div>
-                                        </Card.Footer>
-                                    </Card>
-                                </li>
-                            );
-                        })}
-                    </ul> */}
                 </div>
                 <Card className="resumen">
                     <Card.Body>
@@ -407,22 +327,6 @@ export function VerCarrito() {
                         {/* } */}
                     </Card.Footer>
                 </Card>
-                {/* <ul className="listado-cursos">
-                    {carrito.productos
-                        .map((producto, index) => (
-                            <li key={`${index}`}>
-                                <p><b>{producto.nombre}</b></p>
-                                <p>${producto.precio}</p>
-                                <button
-                                className="btn btn-danger mt-3 me-3"
-                                    type="submit"
-                                    onClick={() => handleClick(producto.id)}
-                                    >
-                                    Quitar
-                                    </button>
-                            </li>
-                            ))}
-                </ul> */}
 
 
                 <Modal show={showModal} onHide={handleCancelar}>
@@ -431,7 +335,6 @@ export function VerCarrito() {
                     </Modal.Header>
                     <Modal.Body>
                         <p>¿Estás seguro de que quieres finalizar la compra?</p>
-                        <p>Productos a comprar: <b>Total: {importeCarrito}</b></p>
                         <ListGroup>
                             {productosComprar.map((producto, index) => (
                                 <ListGroup.Item key={`${producto.id}-${index}`}>
