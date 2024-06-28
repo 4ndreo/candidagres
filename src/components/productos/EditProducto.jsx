fac87082110de75bf972ebe7de218e781379dff2
dreo <79935220+4ndreo@users.noreply.github.com>
2024-06-27 21:05:57 -0300

    refac: edit y create de producto

--- src/components/productos/EditProducto.jsx
+++ src/components/productos/EditProducto.jsx
@@ -1,35 +1,47 @@
 import "../css/Edit.css";
+import "./EditProducto.css";
 import React, { useEffect, useState, useContext } from "react";
 import * as productosService from "../../services/productos.service";
 import * as Constants from "../../Constants";
 import { useNavigate, useParams } from "react-router-dom";
 import { AuthContext } from "../../App";
 
+const imageMimeType = /image\/(png|jpg|jpeg)/i;
+
 export function EditProducto({ title }) {
+  const SERVER_URL = process.env.REACT_APP_SERVER_URL;
   const value = useContext(AuthContext);
+  const [imageError, setImageError] = useState(null);
+  const [file, setFile] = useState(null);
+  const [fileDataURL, setFileDataURL] = useState(null);
 
   let navigate = useNavigate();
   const params = useParams();
 
-  const [nombre, setNombre] = useState("");
-  const [descripcion, setDescripcion] = useState("");
-  const [demora_producto, setDemora] = useState();
-  const [precio, setPrecio] = useState();
-  const [material, setMaterial] = useState();
-  const [icons, setIcons] = useState([]);
+  const [producto, setProducto] = useState({
+    nombre: "",
+    descripcion: "",
+    demora_producto: 0,
+    precio: 0,
+    material: "",
+    imagen: "",
+  });
   const [error, setError] = useState("");
-  const [imagen, setImagen] = useState(null);
-  const [checked, setChecked] = useState({});
 
   useEffect(() => {
     productosService
       .findById(params?.idProducto)
       .then((producto) => {
-        setNombre(producto.nombre);
-        setDescripcion(producto.descripcion);
-        setDemora(producto.demora_producto);
-        setPrecio(producto.precio);
-        setMaterial(producto.material);
+        setProducto(
+          {
+            nombre: producto.nombre,
+            descripcion: producto.descripcion,
+            demora_producto: producto.demora_producto,
+            precio: producto.precio,
+            material: producto.material,
+            img: producto.img,
+          }
+        );
       })
       .catch((err) => setError(err.message));
 
@@ -38,13 +50,34 @@ export function EditProducto({ title }) {
     }
   }, []);
 
+  useEffect(() => {
+    let fileReader, isCancel = false;
+    if (file) {
+      fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const { result } = e.target;
+        if (result && !isCancel) {
+          setFileDataURL(result)
+        }
+      }
+      fileReader.readAsDataURL(file);
+    }
+    return () => {
+      isCancel = true;
+      if (fileReader && fileReader.readyState === 1) {
+        fileReader.abort();
+      }
+    }
+
+  }, [file]);
+
   function handleSubmit(e) {
     e.preventDefault();
     productosService
-      .update(params?.idProducto, { nombre, descripcion, demora_producto, precio, material })
+      .update(params?.idProducto, producto)
       .then((data) => {
-        if (imagen) {
-          return productosService.uploadImagen(imagen).then((nombreImg) => {
+        if (file) {
+          return productosService.uploadImagen(file).then((nombreImg) => {
             productosService.update(params?.idProducto, { img: nombreImg }).then((data) => {
 
             });
@@ -55,83 +88,116 @@ export function EditProducto({ title }) {
       });
   }
 
-  const handleImagenChange = (event) => {
-    const archivo = event.target.files[0];
-    setImagen(archivo);
-    console.log(archivo)
-  };
+  function handleChange(e) {
+    setProducto({ ...producto, [e.target.name]: e.target.value });
+  }
 
-  return (
-    <main className="container edit-cont">
-      <h1>Editar - {title}</h1>
-      <form onSubmit={handleSubmit} className="form">
-        <div className="mb-3">
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Ingrese el nombre del producto</label>
-          <input
-            type="text"
-            defaultValue={nombre}
-            required
-            onChange={(e) => setNombre(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Ingrese la descripción del producto</label>
-          <input
-            type="text"
-            defaultValue={descripcion}
-            required
-            onChange={(e) => setDescripcion(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Defina cuantos dias va a tardar</label>
-          <input
-            type="number"
-            defaultValue={demora_producto}
-            required
-            onChange={(e) => setDemora(parseInt(e.target.value))}
-            className="form-control"
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Cuanto cuesta la clase</label>
-          <input
-            type="number"
-            defaultValue={precio}
-            required
-            onChange={(e) => setPrecio(parseInt(e.target.value))}
-            className="form-control"
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Defina el material del producto</label>
-          <input
-            type="text"
-            defaultValue={material}
-            required
-            onChange={(e) => setMaterial(e.target.value)}
-            className="form-control"
-          />
-        </div>
-        <div className="mb-3">
-          <label className="form-label">Subir Imagen:</label>
-          <input
-            type="file"
-            onChange={handleImagenChange}
-            className="form-control"
-            name="imagenProducto"
-            id="imagenProducto"
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Modificar
-        </button>
-      </form>
-      {error && <p>{error}</p>}
-    </main>
-  );
+  const changeHandler = (e) => {
+    setImageError(null);
+    const file = e.target.files[0];
+    if (!file.type.match(imageMimeType)) {
+      setImageError("El tipo de archivo no es válido");
+      return;
+    }
+    setFile(file);
+  }
+
+  if (producto) {
+
+
+    return (
+      <main className="container edit-cont">
+        <h1>Editar - {title}</h1>
+        <form onSubmit={handleSubmit} className="form">
+          <div className="mb-3">
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Nombre</label>
+            <input
+              type="text"
+              name="nombre"
+              value={producto.nombre}
+              required
+              onChange={(e) => handleChange(e)}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Descripción</label>
+            <input
+              type="text"
+              name="descripcion"
+              value={producto.descripcion}
+              required
+              onChange={(e) => handleChange(e)}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Demora esperada (en días)</label>
+            <input
+              type="number"
+              name="demora_producto"
+              value={producto.demora_producto}
+              required
+              onChange={(e) => handleChange(e)}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Precio</label>
+            <input
+              type="number"
+              name="precio"
+              value={producto.precio}
+              required
+              onChange={(e) => handleChange(e)}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Material</label>
+            <input
+              type="text"
+              name="material"
+              value={producto.material}
+              required
+              onChange={(e) => handleChange(e)}
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Subir Imagen:</label>
+            <input
+              type="file"
+              accept='.png, .jpg, .jpeg'
+              onChange={changeHandler}
+              // onChange={handleImagenChange}
+              className="form-control"
+              name="imagenProducto"
+              id="imagenProducto"
+            />
+            {imageError && <p>{imageError}</p>}
+          </div>
+          <div className="mb-3">
+            {fileDataURL ?
+              <p className="img-preview-wrapper">
+                {<>
+                  <label className="form-label d-block">Nueva imagen:</label>
+                  <img src={fileDataURL} className="product-image img-fluid rounded-3" alt={producto.descripcion} />
+                </>}
+              </p> : <>
+                <label className="form-label d-block">Imagen actual:</label>
+                <img src={SERVER_URL + "uploads/" + producto.img} className="product-image img-fluid rounded-3" alt={producto.descripcion} />
+              </>}
+          </div>
+
+          <button type="submit" className="btn btn-primary">
+            Guardar producto
+          </button>
+        </form>
+        {error && <p>{error}</p>}
+      </main>
+    );
+  }
 }
