2166f217f06c4960e299d0a31ccb663b7d74b924
dreo <79935220+4ndreo@users.noreply.github.com>
2024-09-25 00:53:25 -0300

    fix: reusable connections and useQuery performance fixes

--- src/components/productos/CartProduct/CartProduct.jsx
+++ src/components/productos/CartProduct/CartProduct.jsx
@@ -1,5 +1,5 @@
 import "./CartProduct.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import * as carritoService from "../../../services/carrito.service";
 import { Button } from "react-bootstrap";
 import LoaderMini from "../../basics/LoaderMini";
@@ -7,12 +7,17 @@ import LoaderMini from "../../basics/LoaderMini";
 export function CartProduct({ props }) {
   const SERVER_URL = process.env.REACT_APP_SERVER_URL;
   const [loadingQuantities, setLoadingQuantities] = useState(false);
+  const [quantity, setQuantity] = useState();
   const item = props.item;
+
+  useEffect(() => {
+    setQuantity(props.item.quantity);
+  }, [props.item.quantity]);
 
   const handleAddToCart = async (item) => {
     setLoadingQuantities(true);
     try {
-      props.item.quantity++
+      setQuantity(prev => prev + 1);
       await carritoService.addToCart(props.idUser, { id: item._id, quantity: props.item.quantity });
       props.refetch();
     } catch (err) {
@@ -20,11 +25,12 @@ export function CartProduct({ props }) {
     }
     setLoadingQuantities(false);
   }
-  
+
   const handleSubtractToCart = async (item) => {
     setLoadingQuantities(true);
     try {
-      props.item.quantity--
+      // props.item.quantity--
+      setQuantity(prev => prev - 1);
       await carritoService.substractToCart(props.idUser, { id: item._id, quantity: props.item.quantity });
       props.refetch();
     } catch (err) {
@@ -36,17 +42,14 @@ export function CartProduct({ props }) {
   const renderQuantity = () => {
     return loadingQuantities ?
       <LoaderMini className="loader-mini" /> :
-
-      <div>
+      <>
         <Button variant="danger" onClick={() => handleSubtractToCart(props.item)} disabled={props.loadingQuantities}>-</Button>
 
-        {props.item.quantity || 0}
+        {quantity || 0}
 
         <Button variant="success" onClick={() => handleAddToCart(props.item)
         } disabled={props.loadingQuantities} >+</Button >
-
-      </div>
-
+      </>
   }
 
   return (
@@ -56,16 +59,13 @@ export function CartProduct({ props }) {
       </div>
       <div className="col-10">
         <div className="row g-0 h-100">
-          <div className="d-flex flex-md-column col-md-8 gx-4 justify-content-between  align-self-justify gap-md-4">
+          <div className="d-flex flex-md-column col-md-9 gx-4 justify-content-between  align-self-justify gap-md-4">
             <a href={`/store/item/${item._id}`} className="stretched-link"><span className="card-title">{item.nombre}</span></a>
             <span className="card-price">${item.precio}</span>
           </div>
-          <div className="col-md-4 d-flex flex-column gap-2 align-items-end align-items-md-center justify-content-end">
-
+          <div className="col-md-3 d-flex flex-column gap-2 align-items-end align-items-md-center justify-content-end">
             <div className="counter-cantidad">
-              <div>
-                {renderQuantity()}
-              </div>
+              {renderQuantity()}
             </div>
           </div>
         </div>
