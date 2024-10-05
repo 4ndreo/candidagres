1140818618ba87c929f4de3b433ead4702589172
dreo <79935220+4ndreo@users.noreply.github.com>
2024-10-05 00:03:31 -0300

    feat: paginator working

--- src/pages/admin/AdminProducts/AdminProducts.jsx
+++ src/pages/admin/AdminProducts/AdminProducts.jsx
@@ -4,6 +4,7 @@ import "./AdminProducts.css";
 // React
 import { Link } from "react-router-dom";
 import { useQuery } from "react-query";
+import { useEffect, useState } from "react";
 
 // Services
 import * as productosService from "../../../services/productos.service";
@@ -11,21 +12,62 @@ import * as productosService from "../../../services/productos.service";
 // Components
 import Loader from "../../../components/basics/Loader";
 import AdminProductRow from "../../../components/AdminProductRow/AdminProductRow";
+import Paginator from "../../../components/Paginator/Paginator";
 
 
 export default function AdminProducts() {
-    const fetchPurchases = async () => {
-        return await productosService.find();
+    const [activePage, setActivePage] = useState(1);
+    const [request, setRequest] = useState({
+        page: 0,
+        limit: 2,
+        filter: { field: undefined, value: undefined },
+        sort: undefined,
+        order: 1,
+    });
+
+    const fetchPurchases = async (request) => {
+        const result = await productosService.findQuery(request);
+        return result[0];
     }
 
     const { data: products, isLoading, isError, error, refetch } = useQuery(
-        'products',
-        fetchPurchases,
+        ['products', request],
+        () => fetchPurchases(request),
         {
             staleTime: 1000,
             retry: 2,
         }
     );
+
+    const visiblePages = 3; // show 5 pages at a time
+    const [startPage, setStartPage] = useState(1);
+    const [endPage, setEndPage] = useState(visiblePages + 1);
+    
+
+    function handlePaginate(page) {
+        setActivePage(page + 1);
+        setRequest({ ...request, page: request.limit * page }); //old
+    }
+
+    function handlePaginatePrevious(page) {
+        if (page > 1) {
+            setActivePage(prev => 0 < prev - 1 ? prev - 1 : 1);
+            setStartPage((page - 1) % visiblePages === 0 ? page - visiblePages : startPage);
+            setEndPage((page - 1) % visiblePages === 0 ? activePage : endPage);
+            if (request.page >= request.limit) {
+                setRequest({ ...request, page: request.page - request.limit });
+            }
+        }
+    }
+
+    function handlePaginateNext(page) {
+        setActivePage(prev => prev + 1 <= products.pages ? prev + 1 : products.pages);
+        setStartPage((page) % visiblePages === 0 ? page + 1 : startPage);
+        setEndPage(page % visiblePages === 0 ? activePage + visiblePages + 1 : endPage);
+        if ((request.page + request.limit) <= products.count) {
+            setRequest({ ...request, page: request.page + request.limit });
+        }
+    }
 
     const renderError = () => {
         return (
@@ -62,33 +104,17 @@ export default function AdminProducts() {
                                     <th scope="col">Descripción</th>
                                     <th scope="col">Material</th>
                                     <th scope="col">Precio</th>
-                                    <th scope="col">DEMORA</th>
+                                    <th scope="col">Demora</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {products.map((item) => {
+                                {products.data.map((item) => {
                                     return <AdminProductRow props={{ item: item, refetch: refetch }} key={item._id} />
                                 })}
                             </tbody>
                         </table>
                     </div>
-                    <nav aria-label="pagination">
-                        <ul className="pagination  justify-content-center">
-                            <li className="page-item">
-                                <a className="page-link" href="#" aria-label="Previous">
-                                    <span aria-hidden="true">&laquo;</span>
-                                </a>
-                            </li>
-                            <li className="page-item"><a className="page-link" href="#">1</a></li>
-                            <li className="page-item"><a className="page-link" href="#">2</a></li>
-                            <li className="page-item"><a className="page-link" href="#">3</a></li>
-                            <li className="page-item">
-                                <a className="page-link" href="#" aria-label="Next">
-                                    <span aria-hidden="true">&raquo;</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </nav>
+                    <Paginator props={{ startPage: startPage, endPage: endPage, activePage: activePage, pages: products.pages, page: request.page, limit: request.limit, handlePaginate: handlePaginate, handlePaginateNext: handlePaginateNext, handlePaginatePrevious: handlePaginatePrevious }} />
                 </div>
             }
 
