49f2429c8d869561eea819b4f90dbc0057f26d38
dreo <79935220+4ndreo@users.noreply.github.com>
2024-11-12 22:47:17 -0300

    fix: responsive shifts

--- src/components/ShiftCard/ShiftCard.jsx
+++ src/components/ShiftCard/ShiftCard.jsx
@@ -29,7 +29,7 @@ export default function ShiftCard({ props }) {
   const verifyEnrollment = useCallback(() => {
     return props.shift.enrollments.filter((x) => context.currentUser._id === x.id_user && x.deleted === false);
   }, [props.shift.enrollments, context.currentUser._id]);
-  
+
   useEffect(() => {
     setUserEnrollment(verifyEnrollment()[0]?._id ?? null)
   }, [props.classData, verifyEnrollment])
@@ -46,29 +46,31 @@ export default function ShiftCard({ props }) {
 
   return (
     <>
+      <div className="shift-card-cont border-display">
 
-      <div
-        className={userEnrollment || (props.shift.enrollmentsCount === props.shift.max_places) ? "item-turno w-100 inscripto" : "item-turno w-100"}
-        style={{
-          height: (getDistancia(props.shift.start_time, props.shift.end_time) * factor > factor ? getDistancia(props.shift.start_time, props.shift.end_time) * factor : factor) + "px",
-          top: getDistancia("09:00", props.shift.start_time) * factor + "px",
-          border: "2px solid " + props.hoveredTurno === props.shift._id ? "hsl(220 50% 70% / 1)" : "#e6e6e6",
-          backgroundColor: props.hoveredTurno === props.shift._id ? "hsl(220 50% 70% / 1)" : "#e6e6e6",
-        }}
-        // onClick={() => {handleSelectedTurno(turno); handleCloseTooltip(true);}}
-        onClick={() => setShow(true)}
-        onMouseEnter={() => props.handleMouseOver(props.shift._id)}
-        onMouseLeave={() => props.handleMouseLeave(props.shift._id)}
-      >
-        <div className="d-flex justify-content-between mb-2">
-          <h3>{props.shift.title}</h3>
-          {renderBadge()}
-        </div>
-        <div className="d-flex justify-content-between">
-          <span>{props.shift.start_time}-{props.shift.end_time}hs</span>
-          <span className="d-flex align-items-center gap-1"><span className="pi pi-user"></span>{props.shift.enrollmentsCount}/{props.shift.max_places}</span>
-        </div>
+        <div
+          className={userEnrollment || (props.shift.enrollmentsCount === props.shift.max_places) ? "item-turno w-100 inscripto" : "item-turno w-100"}
+          style={{
+            height: (getDistancia(props.shift.start_time, props.shift.end_time) * factor > factor ? getDistancia(props.shift.start_time, props.shift.end_time) * factor : factor) + "px",
+            top: getDistancia("09:00", props.shift.start_time) * factor + "px",
+            // border: "2px solid " + props.hoveredTurno === props.shift._id ? "hsl(220 50% 70% / 1)" : "#e6e6e6",
+            backgroundColor: props.hoveredTurno === props.shift._id ? "hsl(220 50% 70% / 1)" : "#e6e6e6",
+          }}
+          // onClick={() => {handleSelectedTurno(turno); handleCloseTooltip(true);}}
+          onClick={() => setShow(true)}
+          onMouseEnter={() => props.handleMouseOver(props.shift._id)}
+          onMouseLeave={() => props.handleMouseLeave(props.shift._id)}
+        >
+          <div className="d-flex justify-content-between mb-2 flex-wrap">
+            <h3>{props.shift.title}</h3>
+            {renderBadge()}
+          </div>
+          <div className="d-flex justify-content-between flex-wrap">
+            <span>{props.shift.start_time}-{props.shift.end_time}hs</span>
+            <span className="d-flex align-items-center gap-1"><span className="pi pi-user"></span>{props.shift.enrollmentsCount}/{props.shift.max_places}</span>
+          </div>
 
+        </div>
       </div>
       {show && <EnrollModal props={{
         show, setShow, classData: props.classData, shift: props.shift, weekdays: props.weekdays, userEnrollment, refetch: props.refetch
