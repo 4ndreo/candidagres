b42f3a1463b7aecef3b0d1870b884526789bc8f7
dreo <79935220+4ndreo@users.noreply.github.com>
2024-11-22 22:26:46 -0300

    fix: normal users enrollments

--- src/interceptors/auth.js
+++ src/interceptors/auth.js
@@ -1,27 +1,40 @@
 export const fetchWithInterceptor = async (url, options = {}) => {
     // Agregar el token a los encabezados
     const token = localStorage.getItem('token');
-    if (token) {
-        if (options.headers === undefined) {
-            options.headers = {
-                ...options.headers,
-                'Content-Type': 'application/json',
-                'auth-token': token
-            };
-        } else {
-            options.headers = {
-                ...options.headers,
-                // 'Content-Type': undefined,
-                'auth-token': token
-            };
-
-        }
-    } else {
+    if (options.headers === undefined) {
         options.headers = {
-            ...options.headers,
             'Content-Type': 'application/json',
         };
     }
+    if (token) {
+        options.headers = {
+            ...options.headers,
+            'auth-token': token
+        };
+    }
+    // if (token) {
+    //     if (options.headers === undefined) {
+    //         console.log('no hay headers', token)
+    //         options.headers = {
+    //             ...options.headers,
+    //             'Content-Type': 'application/json',
+    //             'auth-token': token
+    //         };
+    //     } else {
+    //         options.headers = {
+    //             ...options.headers,
+    //             // 'Content-Type': undefined,
+    //             'auth-token': token
+    //         };
+
+    //     }
+    // } else {
+    //     console.log('no hay token', token)
+    //     options.headers = {
+    //         ...options.headers,
+    //         'Content-Type': 'application/json',
+    //     };
+    // }
 
     try {
         const response = await fetch(url, options);
