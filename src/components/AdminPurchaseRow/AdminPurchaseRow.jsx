725f13d74813046d1b3b8a79a02c5ea687529b94
dreo <79935220+4ndreo@users.noreply.github.com>
2024-12-13 01:30:33 -0300

    feat: set purchase as delivered

--- src/components/AdminPurchaseRow/AdminPurchaseRow.jsx
+++ src/components/AdminPurchaseRow/AdminPurchaseRow.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from 'react';
 import './AdminPurchaseRow.css';
-import { Modal } from "react-bootstrap";
+import { Form, Modal, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import * as purchasesService from "../../services/purchases.service";
 import * as mediaService from "../../services/media.service";
@@ -15,25 +15,32 @@ import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
 import { defaultImage } from '@cloudinary/url-gen/actions/delivery';
 
 export default function AdminPurchaseRow({ props }) {
-  const [deleting, setDeleting] = useState();
+  const [delivering, setDelivering] = useState();
+  const [viewing, setViewing] = useState();
   const [show, setShow] = useState(false);
+  const [showView, setShowView] = useState(false);
+  const [errors, setErrors] = useState({});
 
   const handleClose = () => setShow(false);
-  const handleShow = (selected) => { setShow(true); setDeleting(selected) };
+  const handleShow = (selected) => { setShow(true); setDelivering(selected) };
 
-  function handleSelectedDelete(item) {
-    setDeleting(item);
+  function handleSelectedDeliver(item) {
+    setDelivering(item);
   }
 
-  async function handleConfirmDelete(item) {
+  async function handleConfirmDelivery(item) {
     try {
-      await purchasesService.remove(item._id)
-      await mediaService.removeImage(item.img)
-      props.setShowToast({ show: true, title: 'Éxito', message: 'La compra se ha eliminado', variant: 'success', position: 'top-end' });
-      props.refetch();
+      const delivered = await purchasesService.setDelivered(item);
+      if (delivered.err) {
+        setErrors(delivered.err);
+      } else {
+        handleClose();
+        props.setShowToast({ show: true, title: 'Éxito', message: 'La compra se marcó como entregada', variant: 'success', position: 'top-end' });
+        props.refetch();
+      }
     } catch (err) {
-      props.setShowToast({ show: true, title: 'Error al eliminar la compra', message: 'Inténtelo de nuevo más tarde', variant: 'danger', position: 'top-end' });
-
+      props.setShowToast({ show: true, title: 'Error al marcar la compra como entregada', message: 'Inténtelo de nuevo más tarde', variant: 'danger', position: 'top-end' });
+      handleClose();
     }
   }
 
@@ -50,6 +57,11 @@ export default function AdminPurchaseRow({ props }) {
     )
   }
 
+  const TooltipComponent = ({ id, children, title }) => (
+    <OverlayTrigger overlay={<Tooltip id={id}>{title}</Tooltip>}>
+      {children}
+    </OverlayTrigger>
+  );
   return (
     <>
       <tr className="cont-admin-purchases-row">
@@ -59,15 +71,23 @@ export default function AdminPurchaseRow({ props }) {
               return (
                 <td key={index}>
                   <div className="d-flex gap-3 align-items-center justify-content-center">
-                    {props.showEdit && <Link to={props.item._id} aria-label="Editar">
-                      <span className="pi pi-pen-to-square text-primary"></span>
-                    </Link>}
-                    {props.showDelete && <button
-                      onClick={() => { handleShow(); handleSelectedDelete(props.item); }}
-                      className="btn p-0"
+                    {props.showView && <button
+                      onClick={() => { setShowView(true); setViewing(props.item); }}
+                      className="btn p-0 border-0"
                       type="button"
-                      aria-label="Eliminar">
-                      <span className="pi pi-trash text-danger"></span>
+                      aria-label="Ver pedido">
+                      <TooltipComponent title="Ver pedido" id="t-1">
+                        <span className="pi pi-eye text-primary"></span>
+                      </TooltipComponent>
+                    </button>}
+                    {props.showDelivered && !props.item.delivered_at && <button
+                      onClick={() => { handleShow(); handleSelectedDeliver(props.item); }}
+                      className="btn p-0 border-0"
+                      type="button"
+                      aria-label="Confirmar entrega">
+                      <TooltipComponent title="Confirmar entrega" id="t-1">
+                        <span className="pi pi-file-check text-success"></span>
+                      </TooltipComponent>
                     </button>}
                   </div>
                 </td>
@@ -92,32 +112,102 @@ export default function AdminPurchaseRow({ props }) {
           }
         }
         )}
-      </tr>
-      <Modal show={show} onHide={handleClose} size="lg" variant="white" className="modal-delete">
+      </tr >
+      <Modal show={showView} size="lg" variant="white" className="modal-view">
         <Modal.Header className="modal-title" closeButton>
-          <Modal.Title className="negritas">¿Seguro querés eliminar "{deleting?.title}"?</Modal.Title>
+          <Modal.Title className="negritas">Resumen de compra</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
-          <p><span className="negritas">Esta acción es irreversible</span></p>
+        <Modal.Body className='d-flex flex-column gap-4'>
+          <div>
+            <h5>Resumen</h5>
+            <p className='mb-0'><span className='negritas'>Usuario:</span> {viewing?.user?.first_name + ' ' + viewing?.user?.last_name} ({viewing?.user?.email})</p>
+            <p className='mb-0'><span className='negritas'>Importe Total:</span> ${viewing?.totalCost}</p>
+            <p className='mb-0'><span className='negritas'>Cantidad de Items:</span> {viewing?.totalQuantity} artículo{viewing?.totalQuantity > 1 ? 's' : null}</p>
+            <p className='mb-0'><span className='negritas'>Demora estimada:</span> {viewing?.totalDelay} día{viewing?.totalDelay > 1 ? 's' : null}</p>
+          </div>
+          <div className='purchase-items'>
+            <h5>Detalle de artículos</h5>
+            <ul>
+              {viewing?.items?.map((item, index) => (
+                <li key={index}>
+                  <div className="d-flex justify-content-between align-items-center">
+
+                    <div>
+                      <h6 className="mb-0 me-2 d-inline">{item.title}</h6><span className="badge text-bg-primary">{item.material}</span>
+                    </div>
+                    <div><span>{item.quantity} x </span><span>${item.unit_price}</span></div>
+                  </div>
+                  <p>{item.description}</p>
+
+                </li>
+              ))}
+            </ul>
+          </div>
+
         </Modal.Body>
         <Modal.Footer>
           <button
-            onClick={() => { handleClose(); }}
+            onClick={() => { setShowView(false); }}
             className="btn btn-link btn-close-link"
             type="button"
             data-toggle="tooltip"
             data-placement="top">
             <span>Cerrar</span>
           </button>
-          <button
-            onClick={() => { handleConfirmDelete(deleting); handleClose(); }}
-            className="btn btn-danger btn-eliminar"
-            type="button"
-            data-toggle="tooltip"
-            data-placement="top">
-            <span>Eliminar definitivamente</span>
-          </button>
         </Modal.Footer>
+      </Modal>
+      <Modal show={show} onHide={handleClose} size="lg" variant="white" className="modal-deliver">
+        <Modal.Header className="modal-title" closeButton>
+          <Modal.Title className="negritas">¿Ya entregaste este pedido?</Modal.Title>
+        </Modal.Header>
+        <Form onSubmit={(e) => { e.preventDefault(); handleConfirmDelivery({ _id: delivering._id, delivered_at: e.target.delivered_at.value }); }} noValidate>
+          <Modal.Body>
+            <div className='mb-4'>
+              <p className='mb-0'>Seleccioná la fecha en que fue entregado este pedido. El usuario lo verá en su historial.</p>
+              <p className='negritas text-danger'> Esta acción es irreversible.</p>
+              <small className='negritas'>Fecha de entrega: </small>
+              <Form.Control type="date" name="delivered_at" autoFocus placeholder="Fecha de entrega" className="mb-1" />
+              <small className="form-text text-danger">
+                {errors.delivered_at}
+              </small>
+            </div>
+            <div className='purchase-items'>
+              <h5>Detalle de artículos</h5>
+              <ul>
+                {delivering?.items?.map((item, index) => (
+                  <li key={index}>
+                    <div className="d-flex justify-content-between align-items-center">
+
+                      <div>
+                        <h6 className="mb-0 me-2 d-inline">{item.title}</h6><span className="badge text-bg-primary">{item.material}</span>
+                      </div>
+                      <div><span>{item.quantity} x </span><span>${item.unit_price}</span></div>
+                    </div>
+                    <p>{item.description}</p>
+
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button
+              onClick={() => { handleClose(); }}
+              className="btn btn-link btn-close-link"
+              type="button"
+              data-toggle="tooltip"
+              data-placement="top">
+              <span>Cerrar</span>
+            </button>
+            <button
+              className="btn btn-success"
+              type="submit"
+              data-toggle="tooltip"
+              data-placement="top">
+              <span className='pi pi-check'></span><span> Confirmar Entrega</span>
+            </button>
+          </Modal.Footer>
+        </Form>
       </Modal>
     </>
   )
